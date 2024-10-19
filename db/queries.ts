e5204b26d019df74a08d4aa432179e5b90d734de
iculmination <ovsiichuk.bohdan@gmail.com>
2024-10-19 20:07:30 +0300

    stripe + subscription query

--- db/queries.ts
+++ db/queries.ts
@@ -8,6 +8,7 @@ import {
   lessons,
   units,
   userProgress,
+  userSubscription,
 } from "@/db/schema";
 
 export const getUserProgress = cache(async () => {
@@ -203,4 +204,28 @@ export const getLessonPercentage = cache(async () => {
   );
 
   return percentage;
+});
+
+DAY_IN_MS = 86_400_000;
+
+export const getUserSubscription = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userSubscription.findFirst({
+    where: eq(userSubscription.userId, userId),
+  });
+
+  if (!data) {
+    return null;
+  }
+
+  const isActive =
+    data.stripePriceId &&
+    data.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+  return { ...data, isActive: !!isActive };
 });
