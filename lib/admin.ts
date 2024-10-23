317831c2a7598bbd29e9c95e54590f8716f3eb7d
iculmination <ovsiichuk.bohdan@gmail.com>
2024-10-24 00:24:35 +0300

    admins list

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,6 +1,9 @@
 import { auth } from "@clerk/nextjs/server";
 
-const adminsList = ["user_2mkYW7osSDDiPt5UVuDAeqCrZGY"];
+const adminsList = [
+  "user_2mkYW7osSDDiPt5UVuDAeqCrZGY",
+  "user_2nr5VHOBFThMyXBKBSqfLnkKweA",
+];
 
 export const isAdmin = () => {
   const { userId } = auth();
